42c2c973ed11eb3710d42a09654ef1d713e6eafa
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-01-20 23:25:23 +0545

    write a first javascript

---
+++ 3_javascript/1_javascript foundation/1_javascript intro/js1.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+var a=10;
+a=2;
+// console.log(a);
+
+let firstName="prathmesh";
+let age=20;
+let isMarried=false;
+console.log("this person name is"+firstName+ " and their age is " +age)
