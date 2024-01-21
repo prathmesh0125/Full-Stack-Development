d24b0bfcfe1ee6f61dcc02fe80b67b8082513ede
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-01-21 08:49:59 +0545

    if elase and for loop

---
+++ 3_javascript/1_javascript foundation/1_javascript intro/js2.js
@@ -0,0 +1,12 @@
+// if else
+let firstName="prathmesh";
+let age=20;
+let isMarried=true;
+
+if(isMarried){
+  console.log(`${firstName} is married`);
+}
+else{
+  console.log(`${firstName} is not married`);
+
+}
---
+++ 3_javascript/1_javascript foundation/1_javascript intro/js3.js
@@ -0,0 +1,18 @@
+// loop
+let num = 0;
+// while loop
+while (num <= 1000) {
+  // console.log(num);
+  num++;
+}
+// for loop
+for (let i = 0; i <= 1000; i++) {
+  // console.log(i);
+}
+
+// sum from 0 t0 100;
+let ans=0;
+for(let i=0;i<=100;i++){
+  ans+=i;
+}
+console.log(ans);
