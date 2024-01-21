443c09b259820356cc29221d6fc197a5aba74011
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-01-21 15:10:57 +0545

    assignment complete

---
+++ 3_javascript/1_javascript foundation/1_javascript intro/assignments/js1.js
@@ -0,0 +1,6 @@
+let FirstName=prompt("enter your FirstName");
+let LastName=prompt("enter your LastName");
+// let FirstName="prathmesh";
+// let LastName="Bidve";
+
+console.log(`hi ${FirstName} ${LastName} Good Morinng `)
---
+++ 3_javascript/1_javascript foundation/1_javascript intro/assignments/js2.js
@@ -0,0 +1,9 @@
+let gender=prompt("Enter your gender");
+let input=gender.toLocaleLowerCase();
+if(input=="male"){
+  console.log("hello Mr.")
+}
+else{
+  console.log("hello Ms.")
+
+}
