7356ad4c5ef97ad9c6faac1482b3319ba2f44a5e
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-01-24 18:39:57 +0545

    number manuplation

---
+++ 3_javascript/2_javascript Basics/2_number manuplation/js1.js
@@ -0,0 +1,11 @@
+//  parseInt is a built-in function in JavaScript that converts a string to an integer.
+const myString = "123";
+console.log(parseInt(myString));
+const anotherString = "45.67";
+console.log(parseInt(anotherString));
+const anotherString2 = "4567dfds";
+console.log(parseInt(anotherString2));
+
+// parseFloat
+console.log(parseFloat("3.24dsnc"));
+
