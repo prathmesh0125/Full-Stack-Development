470a6dabac5aae1bd3b07d9068c74228a4c4e2d2
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-01-24 22:19:48 +0545

    date object

--- 3_javascript/2_javascript Basics/5_date/js2.js
+++ 3_javascript/2_javascript Basics/5_date/js2.js
@@ -1,16 +1,16 @@
 // creaing small clock on
 function displayTime() {
   const currentDate = new Date();
-
   const hours = currentDate.getHours();
   const minute = currentDate.getMinutes();
   const sec = currentDate.getSeconds();
-  function format(date) {
-    return date < 10 ? `0${date}` : date;
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
   }
-
   console.clear();
-  console.log(`Date: ${format(hours)}:${format(minute)}:${format(sec)}`);
+  console.log(
+    `Date: ${formatTime(hours)}:${formatTime(minute)}:${formatTime(sec)}`
+  );
 }
 
 let clock = displayTime;
