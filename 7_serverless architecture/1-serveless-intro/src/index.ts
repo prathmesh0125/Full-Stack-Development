494a3c9066b83bedb0719831c0488bd622c273bd
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-04-04 08:04:14 +0545

    serverless intro

---
+++ 7_serverless architecture/1-serveless-intro/src/index.ts
@@ -0,0 +1,9 @@
+export interface Env {}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return  Response.json({
+			msg:"hello cloudfare",
+		});
+	},
+};
